c83ffdc3b2ad1c3627b80d781242b38b9aabe800
AAndrzejczuk <agnieszkaandrzejczuk@op.pl>
2020-04-01 00:50:30 +0200

    main menu + task list as dataGrid on 2nd view (colored)

--- IdeasApp/IdeasApp/ViewModels/TasksListViewModel.cs
+++ IdeasApp/IdeasApp/ViewModels/TasksListViewModel.cs
@@ -5,9 +5,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using static IdeasApp.Models.PeopleRepoTest;
 
 namespace IdeasApp.ViewModels {
     public class TasksListViewModel : Screen {
 
+
+        public BindableCollection<PersonModel> People { get; set; }
+
+        public TasksListViewModel() {
+            DataAccess da = new DataAccess();
+            People = new BindableCollection<PersonModel>(da.GetPeople());
+        }
     }
 }
