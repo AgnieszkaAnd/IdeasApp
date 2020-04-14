38cc02b36b8829a87a4c1540247f8ee62660f369
AAndrzejczuk <agnieszkaandrzejczuk@op.pl>
2020-04-14 23:43:38 +0200

    started datagrid view formatting - initial

--- IdeasApp/IdeasApp/ViewModels/MainMenuViewModel.cs
+++ IdeasApp/IdeasApp/ViewModels/MainMenuViewModel.cs
@@ -3,14 +3,13 @@ using System.Data.Common;
 
 namespace IdeasApp.ViewModels {
     public class MainMenuViewModel : Conductor<object>.Collection.OneActive {
+        public static TasksListViewModel taskTableView;
 
         public DbConnection DatabaseConnection { get; set; }
 
         public MainMenuViewModel(DbConnection DBconnection) {
             this.DatabaseConnection = DBconnection;
         }
-
-        public static TasksListViewModel taskTableView;
 
         public void LoadTasksList() {
             taskTableView = new TasksListViewModel(DatabaseConnection);
